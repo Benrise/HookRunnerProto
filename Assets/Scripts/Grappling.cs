a769770b1bdf97128842b3a8243c51b1b15e5ce5
Benrise <benrise_x@mail.ru>
2023-10-25 09:27:56 +0300

    Player movement added

--- Assets/Scripts/Grappling.cs
+++ Assets/Scripts/Grappling.cs
@@ -1,82 +1,82 @@
-using System;
-using System.Threading;
-using System.Security.Cryptography;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System;
+// using System.Threading;
+// using System.Security.Cryptography;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Grappling : MonoBehaviour
-{
-    [Header("References")]
-    private PlayerMovementGrappling pmg;
-    public Transform camera;
-    public Transform gunTip;
-    public LayerMask whatIsGrappleable;
-    public LineRenderer lr;
+// public class Grappling : MonoBehaviour
+// {
+//     [Header("References")]
+//     private PlayerMovementGrappling pmg;
+//     public Transform camera;
+//     public Transform gunTip;
+//     public LayerMask whatIsGrappleable;
+//     public LineRenderer lr;
 
-    [Header("Grappling")]
-    public float maxGrappleDistance;
-    public float grapplDelayTime;
-    public Vector3 grapplePoint;
+//     [Header("Grappling")]
+//     public float maxGrappleDistance;
+//     public float grapplDelayTime;
+//     public Vector3 grapplePoint;
 
-    [Header("Cooldown")]
-    public float grapplingCd;
-    private float grapplingCdTimer;
+//     [Header("Cooldown")]
+//     public float grapplingCd;
+//     private float grapplingCdTimer;
 
-    [Header("Input")]
-    public KeyCode grappleKey = KeyCode.Mouse1;
-    private bool grappling;
+//     [Header("Input")]
+//     public KeyCode grappleKey = KeyCode.Mouse1;
+//     private bool grappling;
 
-    private void Start(){
-        pmg = GetComponent<PlayerMovementGrappling>();
-    }
+//     private void Start(){
+//         pmg = GetComponent<PlayerMovementGrappling>();
+//     }
 
-    private void Update(){
-        if (Input.GetKeyDown(grappleKey)) StartGrapple();
+//     private void Update(){
+//         if (Input.GetKeyDown(grappleKey)) StartGrapple();
 
-        if (grapplingCdTime > 0)
-            grapplingCdTimer -+ Time.deltaTime;
-    }
+//         if (grapplingCdTime > 0)
+//             grapplingCdTimer -+ Time.deltaTime;
+//     }
 
-    private void LateUpdate()
-    {
-        if (grappling)
-            lr.SetPosition(0, gunTip.position);
-    }
+//     private void LateUpdate()
+//     {
+//         if (grappling)
+//             lr.SetPosition(0, gunTip.position);
+//     }
 
-    private void StartGrapple(){
-        if (grapplingCdTimer > 0) return;
+//     private void StartGrapple(){
+//         if (grapplingCdTimer > 0) return;
 
-        grappling = true;
+//         grappling = true;
 
-        RaycastHit hit;
-        if (Phisics.Raycast(camera.position, camera.forward, out hit, maxGrappleDistance, whatIsGrappleable)){
-            grapplePoint = hit.point;
+//         RaycastHit hit;
+//         if (Phisics.Raycast(camera.position, camera.forward, out hit, maxGrappleDistance, whatIsGrappleable)){
+//             grapplePoint = hit.point;
 
-            Invoke(nameof(ExecuteGrapple), grapplDelayTime);
-        }
-        else {
-            grapplePoint = cam. position + cam.forward * maxGrappleDistance;
-            Invoke(nameof(StopGrapple), grapplDelayTime);
-        }
+//             Invoke(nameof(ExecuteGrapple), grapplDelayTime);
+//         }
+//         else {
+//             grapplePoint = cam. position + cam.forward * maxGrappleDistance;
+//             Invoke(nameof(StopGrapple), grapplDelayTime);
+//         }
 
-        lr.enabled = true;
-        lr.SetPosition(1, grapplePoint);
-    }
-
-
-    private void ExecuteGrapple(){
-
-    }
-
-    private void StopGrapple(){
-        grappling = false;
-
-        grapplingCdTimer = grapplingCd;
-
-        lr.enabled = false;
-    }
+//         lr.enabled = true;
+//         lr.SetPosition(1, grapplePoint);
+//     }
 
 
+//     private void ExecuteGrapple(){
 
-}
+//     }
+
+//     private void StopGrapple(){
+//         grappling = false;
+
+//         grapplingCdTimer = grapplingCd;
+
+//         lr.enabled = false;
+//     }
+
+
+
+// }
